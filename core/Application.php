1e40063f84d7204598a757e170ac32273f5b23f1
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-01 14:36:53 +0200

    Edit class Application: set $ROOT_DIR

--- core/Application.php
+++ core/Application.php
@@ -10,17 +10,19 @@ namespace app\core;
  */
 class Application
 {
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
 
-    public function __construct()
+    public function __construct($rootPath)
     {
+        self::$ROOT_DIR = $rootPath;
         $this->request = new Request();
         $this->router = new Router($this->request);
     }
 
     public function run()
     {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
