545049e847fb47054997b2abaffdbe2e294d911d
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-02 00:02:07 +0200

    Add Class RegisterModel

---
+++ models/RegisterModel.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+/**
+ * Class RegisterModel
+ * }
+ * @author Kezimana Aimé Angelo <kezangelo@gmail.com>
+ * @package app\models
+ */
+class RegisterModel extends Model
+{
+    public string $full_name;
+    public string $email;
+    public string $password;
+    public string $confirm_password;
+
+    public function register()
+    {
+        echo "Creating a new user ...";
+    }
+
+    public function rules(): array
+    {
+        return [
+            'full_name' => [
+                self::RULE_REQUIRED
+            ],
+            'email' => [
+                self::RULE_REQUIRED,
+                self::RULE_EMAIL
+            ],
+            'password' => [
+                self::RULE_REQUIRED, [
+                    self::RULE_MIN, 
+                    'min' => 8
+                ], [
+                    self::RULE_MAX, 
+                    'max' => 30
+                ]
+            ],
+            'confirm_password' => [
+                self::RULE_REQUIRED, [
+                    self::RULE_MATCH,
+                    'match' => 'password'
+                ]
+            ],
+        ];
+    }
+}
