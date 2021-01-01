2b25a4f5b78f68e10a0e1b1766f39a28ca3d76b5
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-01 16:29:29 +0200

    Add post method to the contact route

--- public/index.php
+++ public/index.php
@@ -2,12 +2,18 @@
 
 use app\core\Application;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
 
 $app->router->get('/About', 'about');
+
+$app->router->get('/contact', 'contact');
+
+$app->router->post('/contact', function () {
+    return "POST CATCH UP!";
+});
 
 $app->run();
