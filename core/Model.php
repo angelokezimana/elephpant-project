8d314f30eefafc3b07c9e220faa0ecf01f93cba2
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-02 00:00:39 +0200

    Add Class Model

---
+++ core/Model.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Model
+ * 
+ * @author Kezimana Aimé Angelo <kezangelo@gmail.com>
+ * @package app\core
+ */
+abstract class Model
+{
+    public const RULE_REQUIRED = 'required';
+    public const RULE_EMAIL = 'email';
+    public const RULE_MIN = 'min';
+    public const RULE_MAX = 'max';
+    public const RULE_MATCH = 'match';
+
+    public function loadData($data)
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+    }
+
+    abstract public function rules(): array;
+
+    public function validate()
+    {
+        foreach ($this->rules() as $attribute => $rules) {
+            foreach ($rules as $rule) {
+                // todo
+            }
+        }
+    }
+}
