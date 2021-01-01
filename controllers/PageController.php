5959f4fe8f80ad60d47159b71da89348b7d08ce6
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-01 17:32:20 +0200

    Add PageController

---
+++ controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\controllers;
+
+/**
+ * Class PageController
+ * 
+ * @author Kezimana Aimé Angelo <kezangelo@gmail.com>
+ * @package app\controllers
+ */
+class PageController
+{
+    public function saveContact()
+    {
+        return 'Contact saved!';
+    }
+}
