1308b84fec524457a8fe7a55e8c66599bbba4288
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-01 22:19:01 +0200

    Add $controller attribute to the class Application

--- core/Application.php
+++ core/Application.php
@@ -11,20 +11,20 @@ namespace app\core;
 class Application
 {
     public static string $ROOT_DIR;
-    public Router $router;
+    public static Application $app;
     public Request $request;
     public Response $response;
-    public static Application $app;
+    public Router $router;
+    public Controller $controller;
 
     public function __construct($rootPath)
     {
         self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
 
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
-
-        self::$app = $this;
     }
 
     public function run()
