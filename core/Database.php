72a0d03780bfe6ec13de57e7b01cac6c8ba69b65
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-02 15:16:34 +0200

    Add class Database to connect with specified database

---
+++ core/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Database
+ * 
+ * @author Kezimana Aimé Angelo <kezangelo@gmail.com>
+ * @package app\core
+ */
+class Database
+{
+    public \PDO $pdo;
+
+    public function __construct(array $config)
+    {
+        $dsn = $config['dsn'] ?? '';
+        $user = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
+
+        $this->pdo = new \PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+}
