c7043811d175d597015fde1e5d9b8738c19981cf
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-02 21:58:51 +0200

    Add a flash message after a user is registered successfully

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends Controller
             $user->loadData($request->getBody());
 
             if ($user->validate() && $user->save()) {
+                Application::$app->session->setFlash('success', 'Thanks for registering');
                 Application::$app->response->redirect('/');
             }
 
