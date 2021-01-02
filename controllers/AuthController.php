c123f47c4df55dc0c4410c65409c7a1c30c7a36a
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-02 14:19:03 +0200

    Fix errors

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -22,16 +22,23 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        $registerModel = new RegisterModel();
+
         if ($request->isPost()) {
-            $registerModel = new RegisterModel();
             $registerModel->loadData($request->getBody());
 
-            if($registerModel->validate() && $registerModel->register()) {
-
+            if ($registerModel->validate() && $registerModel->register()) {
+                return 'Success';
             }
+
+            return $this->render('register', [
+                'model' => $registerModel
+            ]);
         }
 
         $this->setLayout('auth');
-        return $this->render('register');
+        return $this->render('register', [
+            'model' => $registerModel
+        ]);
     }
 }
