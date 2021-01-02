7cc0b5b38bca633b25efe1cf63dafe848c5c59eb
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-02 15:17:44 +0200

    Add array config as a second parameter to the Application instance

--- public/index.php
+++ public/index.php
@@ -6,7 +6,18 @@ use app\core\Application;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$app = new Application(dirname(__DIR__));
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/', [PageController::class, 'home']);
 
