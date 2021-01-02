5fbf773329123aaafe5ed7bc8d1f93cb917a0cc4
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-02 15:15:41 +0200

    Add Database $db attribute

--- core/Application.php
+++ core/Application.php
@@ -16,8 +16,9 @@ class Application
     public Response $response;
     public Router $router;
     public Controller $controller;
+    public Database $db;
 
-    public function __construct($rootPath)
+    public function __construct($rootPath, array $config)
     {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
@@ -25,6 +26,7 @@ class Application
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+        $this->db = new Database($config['db']);
     }
 
     public function run()
