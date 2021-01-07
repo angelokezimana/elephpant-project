199bdd9824beed53ae9db609a70cf37d6b00bebf
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-07 22:37:32 +0200

    Implement AuthMiddleware

---
+++ core/middlewares/AuthMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\core\middlewares;
+
+use app\core\Application;
+use app\core\exception\ForbiddenException;
+
+/**
+ * Class AuthMiddleware
+ * 
+ * @author Kezimana Aimé Angelo <kezangelo@gmail.com>
+ * @package app\core\middlewares
+ */
+class AuthMiddleware extends BaseMiddleware
+{
+    public array $actions = [];
+
+    public function __construct(array $actions = [])
+    {
+        $this->actions = $actions;
+    }
+
+    public function execute()
+    {
+        if (Application::isGuest()) {
+            if (empty($this->actions) || in_array(Application::$app->controller->action, $this->actions)) {
+                throw new ForbiddenException();
+            }
+        }
+    }
+}
