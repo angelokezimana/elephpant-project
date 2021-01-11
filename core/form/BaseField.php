bc4bd58b107957c4f06f5b0fb2ed1a2c2671a8ee
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-11 02:11:48 +0200

    Implement class BaseField

---
+++ core/form/BaseField.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\core\form;
+
+use app\core\Model;
+
+/**
+ * Class BaseField
+ * 
+ * @author Kezimana Aimé Angelo <kezangelo@gmail.com>
+ * @package app\core\form
+ */
+abstract class BaseField
+{
+    public Model $model;
+    public string $attribute;
+
+    abstract public function renderInput(): string;
+
+    public function __construct(Model $model, string $attribute)
+    {
+        $this->model = $model;
+        $this->attribute = $attribute;
+    }
+
+    public function __toString()
+    {
+        return sprintf(
+            '
+            <div class="form-group">
+                <label>%s</label>
+                %s
+                <div class="invalid-feedback">%s</div>
+            </div>
+            ',
+            $this->model->getLabel($this->attribute),
+            $this->renderInput(),
+            $this->model->getFirstError($this->attribute),
+        );
+    }
+}
