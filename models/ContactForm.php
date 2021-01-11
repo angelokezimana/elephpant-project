b5a88b100974b08f066f3b5604cafd4e2522e138
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-11 02:13:38 +0200

    Implement class ContactForm

---
+++ models/ContactForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+/**
+ * Class ContactForm
+ * 
+ * @author Kezimana Aimé Angelo <kezangelo@gmail.com>
+ * @package app\models
+ */
+class ContactForm extends Model
+{
+    public string $subject = '';
+    public string $email = '';
+    public string $body = '';
+
+    public function rules(): array
+    {
+        return [
+            'subject' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'body' => [self::RULE_REQUIRED],
+        ];
+    }
+
+    public function labels():array
+    {
+        return [
+            'subject' => 'Enter your subject',
+            'email' => 'Your email',
+            'body' => 'Body',
+        ];
+    }
+
+    public function send()
+    {
+        return true;
+    }
+}
