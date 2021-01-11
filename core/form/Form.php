7f0d97815966c7f2a5ddca651ca37453ebf8d83e
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-11 02:11:27 +0200

    Rename class Field to InputField

--- core/form/Form.php
+++ core/form/Form.php
@@ -25,6 +25,6 @@ class Form
 
     public function field(Model $model, $attribute)
     {
-        return new Field($model, $attribute);
+        return new InputField($model, $attribute);
     }
 }
---
+++ core/form/InputField.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\core\form;
+
+use app\core\Model;
+
+/**
+ * Class Field
+ * 
+ * @author Kezimana Aimé Angelo <kezangelo@gmail.com>
+ * @package app\core\form
+ */
+class InputField extends BaseField
+{
+    public const TYPE_TEXT = 'text';
+    public const TYPE_PASSWORD = 'password';
+    public const TYPE_NUMBER = 'number';
+    public const TYPE_TEL = 'tel';
+
+    public string $type;
+
+    public function __construct(Model $model, string $attribute)
+    {
+        $this->type = self::TYPE_TEXT;
+        parent::__construct($model, $attribute);
+    }
+
+    public function passwordField()
+    {
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
+    }
+
+    public function renderInput(): string
+    {
+        return sprintf(
+            '<input type="%s" name="%s" value="%s" class="form-control%s">',
+            $this->type,
+            $this->attribute,
+            $this->model->{$this->attribute},
+            $this->model->hasError($this->attribute) ? ' is-invalid' : '',
+        );
+    }
+}
