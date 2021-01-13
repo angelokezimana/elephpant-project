0a3badd8e5b97264343d3e2ce60096f4f01c5449
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-01-14 00:25:17 +0200

    Fix error: add userClass key in config array

--- migrations.php
+++ migrations.php
@@ -8,6 +8,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $config = [
+    'userClass' => \app\models\User::class,
     'db' => [
         'dsn' => $_ENV['DB_DSN'],
         'user' => $_ENV['DB_USER'],
