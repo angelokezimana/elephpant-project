87c7f76c9cba9ca73d04de81c9afd43a7802a4f2
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-02-04 21:31:08 +0200

    Add ExampleTest.php

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
